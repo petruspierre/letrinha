a50a4ddc2a466e0d9e66905971e71e16f5acbce7
Petrus Bento <petrus.tsx@gmail.com>
2022-02-06 11:10:03 -0300

    Added typescript

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Letrinha</title>
+      </Head>
+      <h1>Letrinha</h1>
+    </>
+  )
+}

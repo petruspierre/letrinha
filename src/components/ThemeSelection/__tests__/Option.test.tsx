7a40eed2ea089907e45d7ea8c4a66ab861b1330a
Petrus Bento <petrus.tsx@gmail.com>
2022-02-06 21:57:10 -0300

    Linting

--- src/components/ThemeSelection/__tests__/Option.test.tsx
+++ src/components/ThemeSelection/__tests__/Option.test.tsx
@@ -1,51 +1,51 @@
-import { render, fireEvent } from '@test/components';
+import { render, fireEvent } from "@test/components";
 
-import Option from '../Option';
+import Option from "../Option";
 
 const defaultProps = {
   active: false,
-  name: 'Theme Option',
+  name: "Theme Option",
   colors: {
-    primary: '#fff',
-    secondary: '#000',
+    primary: "#fff",
+    secondary: "#000",
   },
   onClick: jest.fn(),
 };
 
-describe('Option component', () => {
-  describe('renders sucessfully', () => {
-    it('render component', () => {
+describe("Option component", () => {
+  describe("renders sucessfully", () => {
+    it("render component", () => {
       const { getByText, getByTestId } = render(<Option {...defaultProps} />);
 
-      expect(getByText('Theme Option')).toBeInTheDocument();
-      expect(getByTestId('previewPrimary')).toBeInTheDocument();
-      expect(getByTestId('previewSecondary')).toBeInTheDocument();
+      expect(getByText("Theme Option")).toBeInTheDocument();
+      expect(getByTestId("previewPrimary")).toBeInTheDocument();
+      expect(getByTestId("previewSecondary")).toBeInTheDocument();
     });
 
-    it('render previews', () => {
+    it("render previews", () => {
       const { getByTestId } = render(<Option {...defaultProps} />);
 
-      expect(getByTestId('previewPrimary')).toHaveStyleRule(`background: #fff`);
-      expect(getByTestId('previewSecondary')).toHaveStyleRule(
+      expect(getByTestId("previewPrimary")).toHaveStyleRule(`background: #fff`);
+      expect(getByTestId("previewSecondary")).toHaveStyleRule(
         `background: #000`
       );
     });
 
-    it('render border when active', () => {
+    it("render border when active", () => {
       const { getByText } = render(<Option {...defaultProps} active />);
 
-      const defaultTheme = getByText('Theme Option').parentElement;
+      const defaultTheme = getByText("Theme Option").parentElement;
 
-      expect(defaultTheme).toHaveStyleRule('border-bottom');
+      expect(defaultTheme).toHaveStyleRule("border-bottom");
     });
   });
 
-  it('calls onClick callback', () => {
+  it("calls onClick callback", () => {
     const cb = jest.fn();
 
     const { getByRole } = render(<Option {...defaultProps} onClick={cb} />);
 
-    fireEvent.click(getByRole('menuitem'));
+    fireEvent.click(getByRole("menuitem"));
 
     expect(cb).toHaveBeenCalledTimes(1);
   });
--- src/components/ThemeSelection/__tests__/ThemeSelection.test.tsx
+++ src/components/ThemeSelection/__tests__/ThemeSelection.test.tsx
@@ -1,46 +1,46 @@
-import { render, fireEvent } from '@test/components';
+import { render, fireEvent } from "@test/components";
 
-import ThemeSelection from '..';
+import ThemeSelection from "..";
 
 const defaultProps = {
   dismiss: jest.fn(),
 };
 
-describe('Theme Selection component', () => {
-  describe('renders successfully', () => {
-    it('render all themes', () => {
+describe("Theme Selection component", () => {
+  describe("renders successfully", () => {
+    it("render all themes", () => {
       const { getByText } = render(<ThemeSelection {...defaultProps} />);
 
-      expect(getByText('Light')).toBeInTheDocument();
-      expect(getByText('Dark')).toBeInTheDocument();
-      expect(getByText('Default')).toBeInTheDocument();
+      expect(getByText("Light")).toBeInTheDocument();
+      expect(getByText("Dark")).toBeInTheDocument();
+      expect(getByText("Default")).toBeInTheDocument();
     });
   });
 
-  describe('interacts successfully', () => {
-    it('call dismiss function', () => {
+  describe("interacts successfully", () => {
+    it("call dismiss function", () => {
       const cb = jest.fn();
 
       const { getByRole } = render(<ThemeSelection dismiss={cb} />);
 
-      fireEvent.click(getByRole('button'));
+      fireEvent.click(getByRole("button"));
 
       expect(cb).toHaveBeenCalledTimes(1);
     });
 
-    it('change theme', () => {
+    it("change theme", () => {
       const { getByText } = render(<ThemeSelection {...defaultProps} />);
 
-      const darkTheme = getByText('Dark').parentElement;
-      const defaultTheme = getByText('Default').parentElement;
+      const darkTheme = getByText("Dark").parentElement;
+      const defaultTheme = getByText("Default").parentElement;
 
-      expect(defaultTheme).toHaveStyleRule('border-bottom');
-      expect(darkTheme).not.toHaveStyleRule('border-bottom');
+      expect(defaultTheme).toHaveStyleRule("border-bottom");
+      expect(darkTheme).not.toHaveStyleRule("border-bottom");
 
       fireEvent.click(darkTheme);
 
-      expect(defaultTheme).not.toHaveStyleRule('border-bottom');
-      expect(darkTheme).toHaveStyleRule('border-bottom');
+      expect(defaultTheme).not.toHaveStyleRule("border-bottom");
+      expect(darkTheme).toHaveStyleRule("border-bottom");
     });
   });
 });

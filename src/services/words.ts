ae7f57118bc4c620a3350c8013f03dd2f6924e97
Petrus Bento <petrus.tsx@gmail.com>
2022-02-08 22:28:16 -0300

    Fix timezone

--- src/services/words.ts
+++ src/services/words.ts
@@ -1,12 +1,17 @@
 import { format, startOfDay } from "date-fns";
+import { utcToZonedTime } from "date-fns-tz";
 
 import { DailyWord } from "~/model/SimpleGame";
 import api from "./api";
 
 const getDailyWord = async () => {
   const response = await api.get<DailyWord>("word/daily-word");
+  const todayUTC = startOfDay(new Date());
 
-  const today = format(startOfDay(new Date()), "yyyy-MM-dd");
+  const today = format(
+    utcToZonedTime(todayUTC, "America/Fortaleza"),
+    "yyyy-MM-dd"
+  );
 
   return response.data[today].word;
 };

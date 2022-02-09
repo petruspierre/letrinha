02f0265e7e1b60fd1525b973274fb2bff2cc107b
Petrus Bento <petrus.tsx@gmail.com>
2022-02-09 14:51:31 -0300

    add tests to how to play

---
+++ src/components/HowToPlay/__tests__/HowToPlay.test.tsx
@@ -0,0 +1,25 @@
+import { render, fireEvent } from "@test/components";
+
+import HowToPlay from "..";
+
+const defaultProps = {
+  dismiss: jest.fn(),
+};
+
+describe("HowToPlay component", () => {
+  it("matches snapshot", () => {
+    const { container } = render(<HowToPlay {...defaultProps} />);
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it("calls dismiss callback", () => {
+    const cb = jest.fn();
+
+    const { getByTitle } = render(<HowToPlay {...defaultProps} dismiss={cb} />);
+
+    fireEvent.click(getByTitle("Fechar tutorial"));
+
+    expect(cb).toBeCalledTimes(1);
+  });
+});

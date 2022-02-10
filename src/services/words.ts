8b96387b2c609bf7e2e99e719ea8bc27f67836af
Petrus Bento <petrus.tsx@gmail.com>
2022-02-10 17:24:15 -0300

    Use faunadb to store daily words

--- src/services/words.ts
+++ src/services/words.ts
@@ -1,18 +1,41 @@
-import { format, formatISO } from "date-fns";
+import { format } from "date-fns";
+import { query as q } from "faunadb";
 
-import { DailyWord } from "~/model/SimpleGame";
+// import { decrypt, encrypt } from "~/utils/crypt";
+
 import api from "./api";
+import { fauna } from "./fauna";
+
+interface IDailyWordQueryResponse {
+  ref: any;
+  ts: number;
+  data: {
+    date: string;
+    word: string;
+  };
+}
 
 const getDailyWord = async () => {
-  const response = await api.get<DailyWord>("word/daily-word");
-  const now = new Date();
+  try {
+    const now = new Date();
 
-  const today = format(
-    new Date(now.toLocaleString("en-US", { timeZone: "America/Fortaleza" })),
-    "yyyy-MM-dd"
-  );
+    const today = format(
+      new Date(now.toLocaleString("en-US", { timeZone: "America/Fortaleza" })),
+      "yyyy-MM-dd"
+    );
 
-  return response.data[today].word;
+    const { data } = await fauna.query<IDailyWordQueryResponse>(
+      q.Get(q.Match(q.Index("word_by_date"), q.Casefold(today)))
+    );
+
+    // const todayWordResponse = data.word;
+    // const crypto = encrypt(todayWordResponse);
+    // console.log(crypto, decrypt(crypto));
+
+    return data.word;
+  } catch (err) {
+    throw new Error("Error trying to pull daily word. " + err);
+  }
 };
 
 const getWordList = async (wordLength: number) => {

82fde194488792209018c5fa7a56128fa35c5119
Petrus Bento <petrus.tsx@gmail.com>
2022-02-26 18:41:48 -0300

    fix gtag ts error

--- src/services/gtag.ts
+++ src/services/gtag.ts
@@ -1,5 +1,11 @@
 export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID;
 
+declare global {
+  interface Window {
+    gtag?: any;
+  }
+}
+
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url) => {
   if (typeof window.gtag === "undefined") return;

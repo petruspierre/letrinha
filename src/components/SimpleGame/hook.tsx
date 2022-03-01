c65474a8d172e83c363d2d5948f615943d0614b8
Petrus Bento <petrus.tsx@gmail.com>
2022-03-01 01:31:22 -0300

    Urgent fix

--- src/components/SimpleGame/hook.tsx
+++ src/components/SimpleGame/hook.tsx
@@ -326,8 +326,6 @@ const useGame = ({ dailyWord, wordList }: ISimpleGameHookProps) => {
         );
         let lastGuessIndex = lastFilledGuess.length - 1;
 
-        console.log(lastGuessIndex);
-
         if (
           lastGuessIndex < state.wordLength &&
           lastFilledGuess[lastGuessIndex].every((letter) => letter.letter)
@@ -335,8 +333,6 @@ const useGame = ({ dailyWord, wordList }: ISimpleGameHookProps) => {
           lastGuessIndex = lastGuessIndex + 1;
         }
         const lastGuess = state.guesses[lastGuessIndex];
-
-        console.log(lastGuessIndex, lastGuess);
 
         if (lastGuess) {
           const lastIndex = lastGuess.filter((item) => item.letter).length;
